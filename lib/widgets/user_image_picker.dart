37fb91b22d0cec633a6eaaf1241af7c7d0483473
jainparag1 <42775301+jainparag1@users.noreply.github.com>
2023-07-14 08:38:58 +0800

    second commit

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -1,30 +1,40 @@
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key, required this.onPickImage});
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
+
   final void Function(File pickedImage) onPickImage;
+
   @override
-  State<StatefulWidget> createState() {
+  State<UserImagePicker> createState() {
     return _UserImagePickerState();
   }
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _pickedImageFile;
+
   void _pickImage() async {
     final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
+      source: ImageSource.camera,
       imageQuality: 50,
       maxWidth: 150,
     );
+
     if (pickedImage == null) {
       return;
     }
+
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
+
     widget.onPickImage(_pickedImageFile!);
   }
 
@@ -39,12 +49,15 @@ class _UserImagePickerState extends State<UserImagePicker> {
               _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
         TextButton.icon(
-            onPressed: _pickImage,
-            icon: const Icon(Icons.image),
-            label: Text(
-              'Add Image',
-              style: TextStyle(color: Theme.of(context).colorScheme.primary),
-            ))
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        )
       ],
     );
   }
